c292ea40cd76b96fad15a7e35a85424ca03d25fc
Levi Barker <levi.barker@outlook.com>
2023-03-14 16:46:06 -0500

    direct changes yet again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:351613313600:web:e3b00e91fec4ac947875a5',
     storageBucket: 'pointing-poker-89f59.appspot.com',
     apiKey: 'AIzaSyDFLeuZIAb2phM_fB-HLa6ZiVedVz1dEHc',
-    authDomain: 'pointing-poker-89f59.firebaseapp.com',
+    authDomain: 'pointing.levibarker.com',
     messagingSenderId: '351613313600',
     measurementId: 'G-P50SLYHFHY',
   },
