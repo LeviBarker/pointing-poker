0bb05af9f72d002e0fd176f8e8a60ca027f73b88
Levi Barker <levi.barker@outlook.com>
2023-12-26 12:00:57 -0600

    Refactor codebase to improve organization and maintainability
    The commit encompasses a major refactoring of the code. This includes the reorganization of methods across different services to improve code readability and maintainability. Unnecessary console log statements have been removed. Services have been employed to manage specific tasks, such as feedbacks or room configurations, improving application's structure and promoting separation of concerns. Unnecessary import statements have also been cleaned up to reduce bloat and clutter.

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,43 +1,33 @@
-import { Component } from '@angular/core';
-import {
-  CollectionReference,
-  DocumentData,
-  Firestore,
-  addDoc,
-  collection,
-} from '@angular/fire/firestore';
-import { Router } from '@angular/router';
-import { Room } from 'src/app/models/Room';
-import {getValue, RemoteConfig} from "@angular/fire/remote-config";
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {RoomService} from "@app/services/room.service";
+import {RemoteConfigService} from "@app/services/remote-config.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   roomTitle: string = '';
-  firestore: Firestore;
-  roomCollection: CollectionReference<DocumentData>;
   cardOptions: string = '1,2,3,4,5,6,7,8,9,10';
   possibleCardOptions: string[] = [];
   creatingRoom: boolean = false;
 
-  constructor(firestore: Firestore, private router: Router, private remoteConfig: RemoteConfig) {
-    this.firestore = firestore;
-    this.roomCollection = collection(this.firestore, 'rooms');
-    this.possibleCardOptions = getValue(this.remoteConfig, 'card_options').asString()?.replaceAll('"', '').split(':') || [];
+  constructor(private roomService: RoomService, private router: Router, private remoteConfigService: RemoteConfigService) {
+  }
+
+  async ngOnInit() {
+    this.possibleCardOptions = this.remoteConfigService
+      .getValue('card_options')
+      .asString()
+      ?.replaceAll('"', '')
+      .split(':') || [];
   }
 
   async createRoom() {
     this.creatingRoom = true;
-    const room: Partial<Room> = {
-      name: this.roomTitle,
-      show_cards: false,
-      card_options: this.cardOptions,
-      created_at: new Date().getTime(),
-    };
-    const roomDoc = await addDoc(this.roomCollection, room);
-    this.router.navigate([`/room/${roomDoc.id}`]);
+    const roomDoc = await this.roomService.createRoom(this.roomTitle, this.cardOptions);
+    await this.router.navigate([`/room/${roomDoc.id}`]);
   }
 }
