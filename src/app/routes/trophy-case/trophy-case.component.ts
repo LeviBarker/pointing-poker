004a3f130dd9c4168c0f583d798c3d522a37c44a
Levi Barker <levi.barker@outlook.com>
2025-10-02 11:18:09 -0500

    change trophy case

--- src/app/routes/trophy-case/trophy-case.component.ts
+++ src/app/routes/trophy-case/trophy-case.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { TrophyService } from "@app/services/trophy.service";
+import {Component} from "@angular/core";
+import {TrophyService} from "@app/services/trophy.service";
 
 @Component({
     selector: 'app-room',
@@ -10,11 +10,11 @@ import { TrophyService } from "@app/services/trophy.service";
   <div style="display: flex; gap: 16px; flex-wrap: wrap; align-items: center; justify-content: flex-start;">
   <div *ngFor="let trophy of trophies$ | async" style="height: 156px; display: flex; flex-direction: column; justify-content: flex-end; align-items: center; gap: 8px; max-width: 156px">
     <img [src]="trophy['url']" />
-    <small>Seen {{trophy['date']?.seconds * 1000 | date}} by {{trophy['user']}}'s team</small>
+    <small>Caught {{ trophy['date']?.seconds * 1000 | date }} by {{ trophy['user'] }}'s team.</small>
   </div>
   </div>
-  
-</div>
+
+  </div>
   `,
 })
 export class TrophyCaseComponent {
